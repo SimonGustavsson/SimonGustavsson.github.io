ef1a87bef9580b77ad8214b7a752235120b5c06c
Simon Gustavsson <simongustavsson@driveworks.co.uk>
2019-02-24 11:43:32 -0500

    Deployer that works in theory

--- deploy.js
+++ deploy.js
@@ -3,6 +3,19 @@ const fs = require('fs');
 const path = require('path');
 const os = require('os');
 
+function removeFileAndFolder(tempPath, folder) {
+    fs.unlink(tempPath, err => {
+        if (err) throw err;
+        console.log('Removed ' + tempPath);
+
+        fs.rmdir(folder, err => {
+            if (err) throw err;
+
+            console.log('Deleted ' + folder);
+        });
+    });
+
+}
 
 fs.mkdtemp(path.join(os.tmpdir(), 'foo-'), (err, folder) => {
     if (err) throw err;
@@ -14,24 +27,17 @@ fs.mkdtemp(path.join(os.tmpdir(), 'foo-'), (err, folder) => {
 
         console.log('Copied main.min.js to ' + tempPath);
 
-        /*
-        exec('git checkout foobar', (err, stdout, stderr) => {
+        exec('git checkout gh-pages', (err, stdout, stderr) => {
             if (err) throw new Error(err);
 
-            console.log(stdout);
-        });
-        */
-
-        console.log('Deleting temp folder');
-
-        fs.unlink(tempPath, err => {
-            if (err) throw err;
-            console.log('Removed ' + tempPath);
-
-            fs.rmdir(folder, err => {
+            fs.copyFile(tempPath, 'main.minCOPIED.js', err => {
                 if (err) throw err;
 
-                console.log('Deleted ' + folder);
+                exec('git add -A && git commit -m "New deploy" && git checkout master', function (err2, stdout2, stderr2) {
+                    if (err2) throw new Error(err2);
+
+                    removeFileAndFolder(tempPath, folder);
+                });
             });
         });
     });
